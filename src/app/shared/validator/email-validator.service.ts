89845465b46fdf244782b90fca698d2cf94b7695
Your Name <you@exle.com>
2023-01-11 19:14:39 -0600

    Corrección de instrucciones db.json

--- src/app/shared/validator/email-validator.service.ts
+++ src/app/shared/validator/email-validator.service.ts
@@ -22,7 +22,7 @@ export class EmailValidatorService implements AsyncValidator {
    return this.http.get<any[]>(`http://localhost:3000/usuarios?q=${email}`)
       /* el map nos permite transformar el valor que el observable esta emitiendo y regresar cualquier cosa que nosotros queramos  */
         .pipe(
-          delay(3000),
+          // delay(3000),
           map(res => {
             /* La res siempre será un arreglo, si el arreglo ya viene con algo significa que ese correo ya esta tomado pero si regresa un arreglo vacio significa que no se ha tomado*/
             return (res.length === 0) ? null : {emailTomado: true}
